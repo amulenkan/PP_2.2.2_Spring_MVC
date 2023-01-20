13541e4a88198c377881ffc645104ab9cbfd5e93
amulenkan <115099530+amulenkan@users.noreply.github.com>
2023-01-20 20:52:52 +0300

    getCars

--- PP_2_2_2_spring-mvc-master/PP_2_2_2_spring-mvc-master/src/main/java/web/service/CarServiceImpl.java
+++ PP_2_2_2_spring-mvc-master/PP_2_2_2_spring-mvc-master/src/main/java/web/service/CarServiceImpl.java
@@ -3,7 +3,6 @@ package web.service;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
-
 import org.springframework.stereotype.Component;
 import web.models.Car;
 
